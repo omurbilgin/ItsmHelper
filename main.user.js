4a5f272ad463794df2af849604d6807796dbce64
omurbilgin <57267467+omurbilgin@users.noreply.github.com>
2026-05-28 18:44:55 +0300

    Initial submit.

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    gh
+// @version      v01
+// @description  Helper script for ITSM
+// @author       ...
+// @match        Paste the ITSM URL here.
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=79.60
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
